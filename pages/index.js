ef5e769580c88c74b123fc7655780f77067c8005
tylerchen972 <tylerchen972@gmail.com>
2022-05-26 19:05:33 -0400

    fixed needlessly large search bar, placeholer for account information

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Navbar from '../lib/components/Navbar'
 export default function Home() {
   return (
     
-    <div className="container">
+    <div>
       <Head>
         <title>Vid.io</title>
         <link rel="icon" href="/favicon.ico" />
