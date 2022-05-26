a1d80607a2452380e247a82cae15432a8e626ea0
tylerchen972 <tylerchen972@gmail.com>
2022-05-26 18:44:21 -0400

    bootstrap navbar

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import './Navbar.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
