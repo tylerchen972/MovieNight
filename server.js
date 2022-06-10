39530df71dade36aa728eede0dda239318e69944
tylerchen972 <tylerchen972@gmail.com>
2022-06-09 21:10:03 -0400

    make it better

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,3 +10,8 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+app.use(express.static(path.resolve(__dirname, '../cliet/build')));
+app.get('*', (req, res) => {
+    res.sendFile(pah.resolve(__dirname, '../cliet/build', 'index.js'));
+})
