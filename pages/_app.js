ba9492a10b13de19bcef2dbc1b21df409d47064c
tylerchen972 <tylerchen972@gmail.com>
2022-06-10 11:26:38 -0400

    revert to empty repo

--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-import './Navbar.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
