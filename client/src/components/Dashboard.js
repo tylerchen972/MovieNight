4f0a050d56fb6f1fd043aea5f5fa687fa906d45c
tylerchen972 <tylerchen972@gmail.com>
2022-07-14 15:26:25 -0400

    styling for dashboard page, made card component for each task

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useGlobalContext } from "../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
+import ToDoCard from "./ToDoCard";
 
 const Dashboard = () => {
-    const {user} = useGlobalContext();
+    const {user, completeToDos, incompleteToDos} = useGlobalContext();
     const navigate = useNavigate();
 
     React.useEffect(() => {
@@ -13,7 +14,19 @@ const Dashboard = () => {
     }, [user, navigate]);
     
     return (
-        <h1>Dashboard</h1>
+        <div className="dashboard">
+            <div className="todos">
+                {incompleteToDos.map(toDo => (
+                    <ToDoCard toDo={toDo} key={toDo._id} />
+                ))}
+            </div>
+            {completeToDos.length > 0 && <div className="todos">
+                <h2 className="todos__title">Completed</h2>
+                {completeToDos.map(toDo => (
+                    <ToDoCard toDo={toDo} key={toDo._id} />
+                ))}
+                </div>}
+        </div>
     )
 };
 
---
+++ client/src/components/ToDoCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const ToDoCard = ({toDo}) => {
+    const [content, setContent] = React.useState(toDo.content);
+    const [editing, setEditing] = React.useState(false);
+    const input = React.useRef(null);
+
+    const onEdit = (e) => {
+        e.preventDefault();
+        setEditing(true);
+        input.current.focus();
+    }
+
+    const stopEditing = (e) => {
+        if (e) {
+            e.preventDefault();
+        }
+        setEditing(false);
+        setContent(toDo.content);
+    }
+
+    return (
+        <div className={`todo ${toDo.complete ? 'todo--complete': ""}`}>
+            <input type="checkbox" checked={toDo.complete}></input>
+            <input type="text" ref={input} value={content} readOnly={!editing} onChange={e => setContent(e.target.value)}></input>
+            <div className="todo__controls">
+                {!editing ? (
+                    <>
+                        {!toDo.complete &&<button onClick={onEdit}>Edit</button>}
+                        <button>Delete</button>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={stopEditing}>Cancel</button>
+                        <button>Save</button>
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default ToDoCard;
