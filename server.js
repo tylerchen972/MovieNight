974767b63a0e78f4719d947c7859dbb571f9e3c7
tylerchen972 <tylerchen972@gmail.com>
2022-07-15 17:53:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const authRoute = require("./routes/auth");
-const ToDosRoute = require("./routes/todos");
-
+const toDosRoute = require("./routes/todos");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
+
 app.get("/api", (req, res) => {
-    res.send("Fullstack React Express Server");
+  res.send("Fullstack React Express Server");
 });
 
 app.use("/api/auth", authRoute);
-app.use("/api/todos", ToDosRoute);
+app.use("/api/todos", toDosRoute);
 
-app.use(express.static(path.resolve(__dirname, "./cient/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./cient/build", "index.html"));
-})
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('Connected to database');
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
 
     app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+      console.log(`Server running on port ${process.env.PORT}`);
     });
-}).catch((error)=> {
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
